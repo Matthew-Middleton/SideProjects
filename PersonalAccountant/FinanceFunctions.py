3b8d23339db0b6b8c4b592a233dd061b99194001
MeesterMoustache <36935493+MeesterMoustache@users.noreply.github.com>
2018-04-11 17:03:01 -0400

    Add files via upload

---
+++ PersonalAccountant/FinanceFunctions.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        FinanceFunctions
+# Purpose:     To model a personal accountant and manage personal finances.
+#              no security is being planned for implementation currently
+#
+# Author:      Matthew Middleton
+#
+# Created:     11/04/2018
+# Copyright:   (c) Matthew Middleton 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+class FinanceFunctions:
+
+    def __init__(self):
+        self.data = []
+
+    def getBankInfo(self,name=''):
+    #To take bank info and create a file
+
+
