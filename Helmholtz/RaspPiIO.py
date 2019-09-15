e164afcdc3360d294f9a1aa401df0f9c7af1767f
Matthew-Middleton <36935493+Matthew-Middleton@users.noreply.github.com>
2019-09-14 23:14:40 -0400

    Update RaspPiIO.py

--- Helmholtz/RaspPiIO.py
+++ Helmholtz/RaspPiIO.py
@@ -4,31 +4,61 @@ Created on Mon Sep  9 19:19:49 2019
 
 @author: Matthew Middleton
 """
-from datetime import datetime
 import serial
+import time
 
 class RaspPiIO:
-    def __init__(self):
-        self = self
+    #holds last instance of sort_input_lists() parameters
+    data = []
     
-    def sort_input_lists(self, timedate = list, mag_field_x = list,
+    def __init__(self, data = []):
+        self = self
+        self.data = data
+    
+    """Creates a list by placing all indeces from lists next to each
+    other"""
+    def sort_input_lists(self, time_date = list, mag_field_x = list,
                         mag_field_y = list, mag_field_z = list):
-        #to retrieve timedate as unix time use: 
-        #datetime.fromtimestamp(timedate[i]) were i 
-        #is the index you wish to access
-        timedate.sort()
-        mag_field_x.sort()
-        mag_field_y.sort()
-        mag_field_z.sort()
-        data_list = list(timedate)
-        data_list.append(mag_field_x)
-        data_list.append(mag_field_y)
-        data_list.append(mag_field_z)
-        return data_list
-
-    def output_from_TxD(self, timedate = list, mag_field_x = list,
-                        mag_field_y = list, mag_field_z = list):
-        #to use 
-        port = serial.Serial("/dev/ttyAMA0", baudrate=9600)
+        #resets data and data_size to empty
+        self.data = []
+            
+        #create iterables for each list
+        dates = iter(time_date)
+        mag_x = iter(mag_field_x)
+        mag_y = iter(mag_field_y)
+        mag_z = iter(mag_field_y)
+        #iterate through all items in the lists assuming they're of
+        #the same size. Also, captures the total byte count of elements w/in
+        #each set
+        for index in range(0, len(time_date), 1):
+            temp = next(dates)
+            self.data.append(temp.isoformat(' '))
+            temp = next(mag_x)
+            self.data.append(str(temp))
+            temp = next(mag_y)
+            self.data.append(str(temp))
+            temp = next(mag_z)
+            self.data.append(str(temp))
         return
-        
+    
+    """Writes data with UART TxD port on Raspery Pi"""
+    def output_to_TxD(self, list_data = data):
+        #NOTE: XOFF = 0x13
+        #NOTE: XON = 0x11
+        tx_size = 1
+        #make a string to allow for floats, objects, and other types
+        to_write = ' '.join(map(str, list_data))
+        pi = serial.Serial(port="/dev/ttyAMA0", baudrate=9600, xonxoff=True)
+        #wait for serial lines to open and sync up
+        time.sleep(2)
+        print(pi.name)
+        #transmitting bytes
+        while(tx_size):
+            tx_size = pi.write(to_write)
+            time.sleep(2)
+        pi.close()
+        return
+    
+    
+    
+    
