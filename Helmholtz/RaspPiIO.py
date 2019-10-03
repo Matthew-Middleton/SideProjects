e1a0eaa6d022e068df2cef2b8317890f67999759
Matthew-Middleton <36935493+Matthew-Middleton@users.noreply.github.com>
2019-10-03 19:44:16 -0400

    Update RaspPiIO.py

--- Helmholtz/RaspPiIO.py
+++ Helmholtz/RaspPiIO.py
@@ -4,25 +4,24 @@ Created on Mon Sep  9 19:19:49 2019
 
 @author: Matthew Middleton
 """
-import wiringpi
+#import wiringpi
 from sys import getsizeof
 
 class RaspPiIO:
     
-    def __init__(self, data = []):
+    def __init__(self):
         self = self
-        #holds last instance of sort_input_lists() data
-        self.data = data
-        #bytes in string representation of data
-        self.data_size = 0
+        #number of bytes in string representation of data
+        self.__data_size = 0
     
     """Creates a list by placing all indeces from lists next to each
     other"""
     def sort_input_lists(self, time_date = list, mag_field_x = list,
                         mag_field_y = list, mag_field_z = list):
-        #resets data and data_size to empty
-        self.data = []
-        
+        td_str = ''
+        mag_x_str = ''
+        mag_y_str = ''
+        mag_z_str = ''
         #create iterables for each list
         dates = iter(time_date)
         mag_x = iter(mag_field_x)
@@ -38,28 +37,37 @@ class RaspPiIO:
             hold = temp.isoformat('T')
             #adds the size of the string hold and subtracts the base size of
             #a string object
-            self.data_size += getsizeof(hold) - getsizeof('')
-            #adds the string to the list
-            self.data.append(hold)
+            self.__data_size += getsizeof(hold) - getsizeof('')
+            #adds the string to the bigger string
+            td_str += hold
+            td_str += ' '
             
             temp = next(mag_x)
             hold = str(temp)
-            self.data_size += getsizeof(hold) - getsizeof('')
-            self.data.append(hold)
+            self.__data_size += getsizeof(hold) - getsizeof('')
+            mag_x_str += hold
+            mag_x_str += ' '
             
             temp = next(mag_y)
             hold = str(temp)
-            self.data_size += getsizeof(hold) - getsizeof('')
-            self.data.append(hold)
+            self.__data_size += getsizeof(hold) - getsizeof('')
+            mag_y_str += hold
+            mag_y_str += ' '
             
             temp = next(mag_z)
             hold = str(temp)
-            self.data_size += getsizeof(hold) - getsizeof('')
-            self.data.append(hold)
-        return
+            self.__data_size += getsizeof(hold) - getsizeof('')
+            mag_z_str += hold
+            mag_z_str += ' '
+            
+        str_list = list(td_str)
+        str_list.append(mag_x_str)
+        str_list.append(mag_y_str)
+        str_list.append(mag_z_str)
+        return str_list
     
     """Writes data with UART TxD port on Raspery Pi"""
-    def output_to_TxD(self, list_data = list):
+    def output_to_TxD(self, time_date = str, mag_field_x = str, mag_gield_y =str):
         #make a string to allow for multiple data types(strings, int, and float)
         to_write = ' '.join(map(str, list_data))
         wiringpi.wiringPiSetup()
@@ -72,6 +80,8 @@ class RaspPiIO:
         wiringpi.serialClose(serial)
         return
     
-    
+    #getter for data size
+    def data_size(self):
+        return self.__data_size
     
 """send string of each list, start bits, bits for flag sigaling x,y, or z, stop bits"""
